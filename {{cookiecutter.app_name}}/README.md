a7bad497bcf0b7d0a39f13364aecac4c1beb9ac9
simorex80 <82770876+simorex80@users.noreply.github.com>
2024-01-10 10:41:43 +0100

    Add files via upload

---
+++ {{cookiecutter.app_name}}/README.md
@@ -0,0 +1,13 @@
+# {{cookiecutter.project_name}}
+
+# Requirements
+
+* JDK version 17+
+
+## Compile and run tests
+
+```
+mvn clean compile test
+```
+
+In a browser go to `http://localhost:8080/`.
---
+++ {{cookiecutter.app_name}}/src/test/java/com/{{cookiecutter.app_name}}/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.{{cookiecutter.app_name}};
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
